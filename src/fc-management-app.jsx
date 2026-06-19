236d7f75e368a1eda4e71fbce0f518ff31fca75c
fafah-lang <fafah@groupe-venom.fr>
2026-06-19 02:15:46 +0200

    Fix unused vars

--- src/fc-management-app.jsx
+++ src/fc-management-app.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function FCManagementApp() {
-  const [fcs, setFcs] = useState([]);
-
   return React.createElement('div', { style: { padding: '20px' } },
     React.createElement('h1', { style: { color: '#7D1E1C' } }, 'Gestion FC'),
     React.createElement('p', null, 'OHM Energie x Groupe Venom'),
-    React.createElement('p', null, 'App working!')
+    React.createElement('p', null, 'Application en cours de développement')
   );
 }
